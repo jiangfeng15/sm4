4d2b33a73ab42c08798174a4f38a71e4bf139349
jyf <jiangyingfeng163@163.com>
2024-08-19 09:56:13 +0800

    国密算法sm4

---
+++ gm_Algorithm/gm_Algorithm/gm_Algorithm.cpp
@@ -0,0 +1,37 @@
+﻿// gm_Algorithm.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include "pch.h"
+#include <iostream>
+#include "sms4.h"
+
+int main()
+{
+	UCHAR plain_text[16] = {0x01,0x23,0x45,0x67,0x89,0xab,0xcd,0xef,0xfe,0xdc,0xba,0x98,0x76,0x54,0x32,0x10};
+	UCHAR encropty_key[16] = { 0x01,0x23,0x45,0x67,0x89,0xab,0xcd,0xef,0xfe,0xdc,0xba,0x98,0x76,0x54,0x32,0x10 };
+
+	UCHAR cipher_text[16] = { 0x00 };
+
+	
+	SM4_CONTEXT *sm4_context = new SM4_CONTEXT();
+	init_sm4_context(sm4_context, encropty_key, SM4_ENCRYPT,0);
+
+	run_sm4F_once(sm4_context, SM4_ENCRYPT, plain_text, cipher_text);
+
+	memset(plain_text, 0x00, sizeof(plain_text));
+
+	run_sm4F_once(sm4_context, SM4_DECRYPT, cipher_text, plain_text);
+
+	return 0;
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门提示: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
---
+++ gm_Algorithm/gm_Algorithm/sms4.h
@@ -0,0 +1,74 @@
+#pragma once
+#include <stdio.h>
+#include <string.h>
+/*
+* write by jiangyingfeng 
+* 2024.8.16
+*/
+
+#ifndef unlong
+typedef unsigned int unlong;
+#endif /* unlong */
+
+#ifndef unchar
+typedef unsigned char unchar;
+#endif /* unchar */
+
+#ifndef ULONG
+#define ULONG unsigned long
+#endif
+#ifndef UCHAR
+#define UCHAR unsigned char
+#endif
+#ifndef UINT
+#define UINT unsigned int
+#endif
+
+//左旋转操作，例如12 34 56 78->34 56 78 12
+#define SM4CROL(uval, bits) (((uval) << (bits)) | ((uval) >> (0x20 - (bits))))
+/* define MASK code for selecting expected bits from a 32 bits value */
+#define SM4_MASK3 0xFF000000
+#define SM4_MASK2 0x00FF0000
+#define SM4_MASK1 0x0000FF00
+#define SM4_MASK0 0x000000FF
+
+#define SM4_ENCRYPT     0
+#define SM4_DECRYPT     1
+
+typedef struct sm4_key
+{
+	ULONG ERK[32];  /*rk for encrypt*/
+	ULONG DRK[32];  /*rk for decrypt, DRK[31]=ERK[0], ... ,DRK[0]=ERK[31]*/
+	ULONG MK[4];    /*sm4 key 加密密钥*/
+}SM4_KEY;
+
+typedef struct sm4_context {
+	SM4_KEY sm_key;
+
+	UCHAR mode; // 0:ECB,1:CBC
+	UCHAR iv[16]; //CBC use
+	UCHAR left[16]; //last left data
+	UCHAR flag; //1: can decrypt
+	UCHAR left_size; //size of left
+	UCHAR padding_type; //0: padding 0 or not padding, 1: PKCS5 padding
+
+}SM4_CONTEXT;
+
+void init_sm4_rk(SM4_KEY *keyObj);
+ULONG calc_sm4_rk(ULONG a);
+int init_sm4_context(SM4_CONTEXT *key_obj, UCHAR *key,UCHAR mode, UCHAR padding_type);
+
+//合成置换，由非线性变换t和线性变换L复合而成，T(.)=L(t(.)),.表示32位数据
+static ULONG SM4Lt(ULONG a);
+//轮函数，采用非线性迭代结构，以字为单位进行加密运算，称一次迭代运算为一轮变换，其中调用了合成置换
+static ULONG SM4F(ULONG x0, ULONG x1, ULONG x2, ULONG x3, ULONG rk);
+
+/*
+* brief 执行一次F轮函数
+*  param key_obj to be initialized
+*  param flag  SM4_ENCRYPT or SM4_DECRYPT
+*  param input  in 
+*  param output out
+*/
+
+void run_sm4F_once(SM4_CONTEXT *key_obj, UCHAR flag, UCHAR *input, UCHAR * output);
